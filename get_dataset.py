90bf20239dc51cdcb74d29eea883bbcee8689d6f
jy17347 <64970754+jy17347@users.noreply.github.com>
2022-07-12 16:04:13 +0900

    [#1] add : get_dataset

--- get_dataset.py
+++ get_dataset.py
@@ -1,7 +1,29 @@
 import pandas as pd
 import numpy as np
+import os
 
-data_dir = 'Accelerometer.csv'
-dataset = pd.read_csv(data_dir)
 
-print(dataset)
+class IMU_dataset():
+    
+    def __init__(self, folder):
+        
+        data = []
+
+        pose_list = os.listdir(folder)
+        for pose in pose_list:
+            data_dir = folder + '/' + pose + '/Accelerometer.csv'
+            dataset = pd.read_csv(data_dir).to_numpy()
+            data.append((dataset, pose))
+
+        self.data = data
+
+
+    def __length__(self):
+        return len(self.data)
+
+    
+    def __getitem__(self, index):
+
+        data = self.data[index]
+
+        return data
