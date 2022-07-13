e2010b2c387555613e05ea9307b30cd291368c83
jy17347 <64970754+jy17347@users.noreply.github.com>
2022-07-13 17:16:30 +0900

    no message

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import load_model
 
 model_list = os.listdir('./models')
 model_dir = 'models/'+model_list[-1]+'/model.h5'
+
 model = load_model(model_dir)
 model.evaluate(x_test, y_test)
 print('complete')
