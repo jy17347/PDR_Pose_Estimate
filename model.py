b78b6e17e816250c32aec4b0bbe24cdae5715967
jy17347 <64970754+jy17347@users.noreply.github.com>
2022-07-18 20:50:26 +0900

    [#2] ci : model structure

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import Sequential
 def LSTM_model(num_degree, num_pose):
 
     model = Sequential([
-        LSTM(64, activation='relu', return_sequences=True, input_shape=(30,3)),
+        LSTM(64, activation='relu', return_sequences=True, input_shape=(20,6)),
         LSTM(128, activation='relu', return_sequences=True),
         LSTM(128, activation='relu'),
         Dense(64, activation='relu'),
