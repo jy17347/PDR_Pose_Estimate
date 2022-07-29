7e2966f190cb923c1d7f93d88b1cd65d1fe01083
jy17347 <64970754+jy17347@users.noreply.github.com>
2022-07-29 17:12:44 +0900

    Init: React

--- jsconverter.py
+++
@@ -1,8 +0,0 @@
-import tensorflowjs as tfjs
-from tensorflow.keras.models import load_model
-import os
-
-
-models = os.listdir('./models')
-model = load_model(f"models/{models[-1]}/model.h5")
-tfjs.converters.save_keras_model(model, f'models/{models[-1]}/' + '_model_tfjs_')
